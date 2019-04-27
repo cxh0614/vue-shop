8a64e4d7b931be21c614184a0a28006e8e9a9a19
Ly <626947388@qq.com>
2019-04-27 11:44:28 +0800

    异步显示当前地址 & ShopList异步显示 & 异步显示轮播播

---
+++ src/components/Star/Star.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    score: Number,
+    size: Number
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+
+</style>
